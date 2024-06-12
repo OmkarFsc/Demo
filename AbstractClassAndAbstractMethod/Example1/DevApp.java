c5d9be9b9759a729cc6a769d772913983973fa7d
anjana-s-sajeevan <anjana.sajeev@frugalscientific.com>
2024-06-12 16:39:31 +0530

    made changes in DevApp

--- AbstractClassAndAbstractMethod/Example1/DevApp.java
+++ AbstractClassAndAbstractMethod/Example1/DevApp.java
@@ -5,19 +5,11 @@ public class DevApp extends ProjectApp {
     public DevApp() {
         System.out.println("DevApp.DevApp()");
     }
-
-    /**
-     * Implementation for concrete method / instance method (Optional)
-     */
     @Override
     public void task1() {
         System.out.println("DevApp.task1()");
         System.out.println("Task 1 is completed");
     }
-
-    /**
-     * Implementation for abstract method (Mandatory)
-     */
     @Override
     public void task2() {
         System.out.println("DevApp.task2()");
